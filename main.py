cafbb4ea852bad44ff6d0d943dc5a5a2c6f06222
jstoppelman <jstoppelman@comcast.net>
2024-05-16 04:25:41 +0000

    Initial update for new code

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
 #!/usr/bin/env python 
 import numpy as np
+from ase.io import read
+from basis import Basis
 from scf_functions import *
+import sys
 
 def main():
     max_iter = 50
     E_conv = 1e-7
     P_conv = 1e-3
 
-    #Define coordinates in array
-    coords = np.asarray([[0.000, 0.000, 0.000], [1.4632, 0.000, 0.000]])
-    distances = shift_distances(coords)
-    
-    #Define elements
-    elems = ['He', 'H']
-    #For now, store basis set for STO-3G in a dictionary
-    #basis_dict_sto3g = {'H': [[[0.444635, 0.168856], [0.535328, 0.623913], [0.154329, 3.42525]]], 'He': [[[0.44463454, 0.31364979], [0.535328, 1.15892300], [0.15432897, 6.36242139]]]}
-    basis_dict_sto3g = {'H': [[[0.444635, 0.168856], [0.535328, 0.623913], [0.154329, 3.42525]]], 'He': [[[0.444635, 0.480844], [0.535328, 1.77669], [0.154329, 9.75393]]]}
-    basis_dict_631g = {'H': [[[0.0334946, 18.7311370], [0.23472695, 2.8253937], [0.81375733, 0.6401217]], [1.0, 0.1612778]], 'He': [[[0.0237660, 38.421634], [0.1546790, 5.7780300], [0.4696300, 1.2417740]], [1.0, 0.2979640]]}
-    #Nucleus charge
-    charge_dict = {'H': 1.0, 'He': 2.0}
+    atoms = read(sys.argv[1])
 
-    #Get basis and charge for each atom
-    basis = []
-    charge = []
-    nuc_charge = 0
-    elem = 0
-    for i in elems:
-        for func in basis_dict_sto3g[i]:
-            basis.append(Basis_Function(func, distances[elem]))
-        charge.append(charge_dict[i])
-        nuc_charge = nuc_charge + charge[-1]
-        elem += 1
+    #Define elements
+    elems = atoms.get_chemical_symbols()
+
+    #Handle basis set information
+    basis = Basis(atoms, 'sto-3g')
     
     #Determines the number of electrons in the molecule
-    total_charge = 1.0
-    N = int(nuc_charge - total_charge)
-    n_orb = int(N/2)
+    total_charge = basis.get_total_charge()
+    nuclear_charge_sum = basis.get_nuclear_charge()
+    #N is the number of electrons
+    N = nuclear_charge_sum - total_charge
+    sys.exit()
 
     #Define S, T, V and ERI matrices
     s_matrix = np.zeros((len(basis), len(basis)))
